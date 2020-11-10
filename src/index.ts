9d3a15c8d4afb6a2001b28460328c72aa19629f6
Nick Stenroos-Dam <Nisd@project.bi>
2020-11-10 20:42:21 +0100

    Added style sheets and bootstrap

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "./index.css";
 import { ScrapWallet, PickaxeWallet } from "./Wallet";
 import { ManualLabor } from "./ManualLabor";
 
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: 'ts-loader'
             },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            }
         ],
     },
     resolve: {
